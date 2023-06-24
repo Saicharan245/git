d52d37cc882e56ad355c8fc1beea5b990ec3ae01
saicharan <smuthyapwar1995@gmail.com>
2023-06-24 21:10:05 +0100

    message

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -10,4 +10,13 @@ test('Login test demo', async () => {
     await page.goto("https://ecommerce-playground.lambdatest.io/")
     await page.hover("//a[@data-toggle= 'dropdown']//span[contains(.,'My account')] ")
     await page.click("text=Login")
+
+    await page.fill("input[name='email']", "chakisai111@gmail.com")
+
+     await page.fill("input[name='password']", "Nagamani09@")
+     await page.click("input[value='Login']");
+     await page.waitForTimeout(5000);
+
+
+
 })
